bb09b38fd689debe1b5f5d7d7d63d2614439512e
Lakshman Roy <121234657+lakshmanroy@users.noreply.github.com>
2023-10-15 00:20:07 +0530

    Uploaded

---
+++ StringDataType.kt
@@ -0,0 +1,25 @@
+fun main(){
+    // var a:String="Hello kotlin"
+    // var b:String="Language"
+    
+    // var age:Int=25
+
+    // println(a + " " + b)
+
+    // println("your age is " + age)
+
+    var x:String="Lakshman"
+    // var i:Int=x.length
+
+    // println("your lenght of " + i)
+
+    // val b:Boolean=x.equals("java")
+    // println(b)
+
+    // println(x.isEmpty)
+
+    println(x.lowercase())
+    println("*************************88")
+    println(x.uppercase())
+
+}
