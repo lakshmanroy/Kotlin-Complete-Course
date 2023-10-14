b723a0e0fd42636138478637abb7ab49c81aed8d
Lakshman Roy <121234657+lakshmanroy@users.noreply.github.com>
2023-10-14 14:42:59 +0530

    Updated

---
+++ DataTypes.kt
@@ -0,0 +1,19 @@
+fun main(){
+    var a:Boolean=true
+    var b:Char='P'
+    var c:Byte=13
+    var d:Short=-456
+    var e:Int=3456
+    var f:Long=-346789765L
+    var g:Float=5.3346F
+    var h:Double=7.546644
+
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(g)
+    println(h)
+}
