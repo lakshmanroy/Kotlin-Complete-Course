0faa248d34c8b97db47d265aa7463ea220844eac
Lakshman Roy <121234657+lakshmanroy@users.noreply.github.com>
2023-10-14 15:08:38 +0530

    Uploaded

---
+++ TypeConversion.kt
@@ -0,0 +1,27 @@
+/*
+toByte()
+toString()
+toInt()
+toLong()
+toFloat()
+toDouble
+ */
+
+fun main(){
+    // var x:Byte=23
+    // var y:Int=x.toInt()
+    // var z:Double=y.toDouble()
+
+    // println(x)
+    // println(y)
+    // println(z)
+
+    var a:Double=23.44
+    var b:Int=a.toInt()
+    var c:Float=b.toFloat()
+
+    println(a)
+    println(b)
+    println(c)
+
+}
