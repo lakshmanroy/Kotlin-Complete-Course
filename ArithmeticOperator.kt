49a6fce255b0c04bb2cea0a3cc7670889c464f37
Lakshman Roy <121234657+lakshmanroy@users.noreply.github.com>
2023-10-18 00:58:12 +0530

    Uploaded

---
+++ ArithmeticOperator.kt
@@ -0,0 +1,27 @@
+package com.lkrd.chatgpt
+
+fun main() {
+    var number1:Double=8.4
+    var number2:Double=4.2
+    var result:Double=0.0
+
+    result= number1 + number2
+
+    println("number1 + number2=" + result)
+
+    result= number1 - number2
+
+    println("number1 - number2=" + result)
+
+    result= number1 * number2
+
+    println("number1 * number2=" + result)
+
+    result= number1 / number2
+
+    println("number1 / number2=" + result)
+
+    result= number1 % number2
+
+    println("number1 % number2=" + result)
+}
