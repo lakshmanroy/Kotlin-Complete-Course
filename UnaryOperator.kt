4b8d85751635e05919a11ed6c77941d43bfe99f7
Lakshman Roy <121234657+lakshmanroy@users.noreply.github.com>
2023-10-20 00:47:59 +0530

    Uploaded

---
+++ UnaryOperator.kt
@@ -0,0 +1,22 @@
+package com.lkrd.chatgpt
+
+fun main() {
+
+    var number:Double=7.6
+    var isCheck:Boolean=true
+
+    println("+number = " + +number)
+    println("-number = " + -number)
+    println("++number = " + ++number)
+    println("--number = " + --number)
+    println("!isCheck = " + !isCheck)
+
+    println("----------------------------")
+
+    var result:Double= 4.7
+    println("result++ = " +result++)
+    println("result = " +result)
+    println("++result = " + ++result)
+    println("result = " +result)
+
+}
