b99b080238f95d7815388594870042a3c779b1e7
Lakshman Roy <121234657+lakshmanroy@users.noreply.github.com>
2023-10-19 21:08:30 +0530

    Uploaded

---
+++ AssignmentOperator.kt
@@ -0,0 +1,23 @@
+package com.lkrd.chatgpt
+
+fun main() {
+    var a:Int=40
+    var b:Int=20
+    var c:Int=0
+
+    c=a+b
+    println("c = a+b:" + c)
+
+    c +=b
+    println("c +=b:" +c)
+
+    c==b
+    println("c==b:" + c)
+
+    c -=b
+    println("c -=b:" + c)
+
+    c %=b
+    println("c %=b:" +c)
+
+}
