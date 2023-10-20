26f236a8da7caa1ee8435b71598c6bc7f763ffb8
Lakshman Roy <121234657+lakshmanroy@users.noreply.github.com>
2023-10-20 13:10:46 +0530

    Uploaded

---
+++ EqualityOperator.kt
@@ -0,0 +1,13 @@
+package com.example.testapplication
+
+fun main() {
+    var a:Int=6
+    var b:Int=8
+
+    println("a==b : " +(a==b))
+    println("a!=b : " +(a!=b))
+    println("a>=b : " +(a>=b))
+    println("a>b : " +(a>b))
+    println("a<b : " +(a<b))
+    println("a<=b : " +(a<=b))
+}
