1068aed794d0254c1986b0266ffc5e3cbeb0ee9e
Lakshman Roy <121234657+lakshmanroy@users.noreply.github.com>
2023-10-20 13:34:53 +0530

    Uploaded

---
+++ OperatorPrecedence.kt
@@ -0,0 +1,13 @@
+package com.example.testapplication
+
+fun main() {
+
+   var result:Int= 5+4*4
+
+    println("result = " +result)
+
+    var result1:Int= (5+4)*4
+
+    println("result = " +result1)
+
+}
