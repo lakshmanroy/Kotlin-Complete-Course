9efa05de0c6863df003a1a782be92bb9922d683a
Lakshman Roy <lkrd5115@gmail.com>
2023-10-21 15:22:27 +0530

    Uploaded

--- TypeConversion.kt
+++ TypeConversion.kt
@@ -8,13 +8,13 @@ toDouble
  */
 
 fun main(){
-    // var x:Byte=23
-    // var y:Int=x.toInt()
-    // var z:Double=y.toDouble()
+    var x:Byte=23
+    var y:Int=x.toInt()
+    var z:Double=y.toDouble()
 
-    // println(x)
-    // println(y)
-    // println(z)
+    println(x)
+    println(y)
+    println(z)
 
     var a:Double=23.44
     var b:Int=a.toInt()
